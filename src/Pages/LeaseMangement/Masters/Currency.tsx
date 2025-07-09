16f3bf2775a0f2ca10c0f997032d8d3676ccfd6d
MaitriAI-team <dev2.team@worldoffinrep.com>
2025-07-09 14:33:18 +0530

    change save btn text in Currency

--- src/Pages/LeaseMangement/Masters/Currency.tsx
+++ src/Pages/LeaseMangement/Masters/Currency.tsx
@@ -229,7 +229,7 @@ const Currency: React.FC = () => {
                   type="submit"
                   className="flex-1 bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700"
                 >
-                  {editingAsset ? "Update Asset" : "Save Asset"}
+                  {editingAsset ? "Update" : "Save"}
                 </button>
                 <button
                   type="button"
